ce8dddd3bff47820c9bf89d754ddbad1c637e005
Luke Van In <lukevanin@gmail.com>
2022-05-28 12:20:39 +0200

    Initial Commit

---
+++ ClearScore/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ClearScore
+//
+//  Created by Luke Van In on 2022/05/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
